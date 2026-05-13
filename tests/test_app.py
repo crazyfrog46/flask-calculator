e8c95b58f9005b06ac18b86bc1f2a6fe3db3cac2
BalajiS-Srikanthan <x23413263@student.ncirl.ie>
2026-05-13 10:45:11 +0100

    check 6

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,13 @@
+import sys
+import os
+
+# Ensure project root is in Python path (important for Jenkins/CI)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
+
 
 def test_add():
     with app.test_client() as c:
-        rv = c.post("/", data={"a":"2","b":"3","op":"add"})
+        rv = c.post("/", data={"a": "2", "b": "3", "op": "add"})
         assert b"Result: 5.0" in rv.data
